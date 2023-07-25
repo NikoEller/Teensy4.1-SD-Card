8c6ccd6f4196e4150639f3a21f071d46e99dc7ae
NikoEller <niko.eller@live.com>
2023-07-25 16:45:45 +0200

    added card info function

--- CalculateAndSaveToSDCard/CalculateAndSaveToSDCard.ino
+++ CalculateAndSaveToSDCard/CalculateAndSaveToSDCard.ino
@@ -39,11 +39,17 @@ void setup() {
     return;
   }
   Serial.println("initialization done.");
-  SD.remove("data.txt");
+  cardinfo();
 
+  
+  SD.remove("data.txt");
+  
 
 }
 
+/*
+
+*/
 void loop() {
 
   // put your main code here, to run repeatedly:
@@ -59,7 +65,9 @@ void loop() {
 }
 
 
+/*
 
+*/
 void saveData(String data){
   //open the file
   myFile = SD.open("data.txt", FILE_WRITE);
@@ -78,6 +86,9 @@ void saveData(String data){
   }
 }
 
+/*
+
+*/
 void readData(){
   //open the file
   myFile = SD.open("data.txt", FILE_READ);
@@ -97,6 +108,9 @@ void readData(){
 
 }
 
+/*
+
+*/
 void blink() {
   digitalWrite(buildInLEDPin, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(100);               
@@ -105,8 +119,51 @@ void blink() {
 
 }
 
-void cardinfo(){
+/*
 
+*/
+void cardinfo(){
+  // print the type of card
+  Serial.print("\nCard type: ");
+  switch(card.type()) {
+    case SD_CARD_TYPE_SD1:
+      Serial.println("SD1");
+      break;
+    case SD_CARD_TYPE_SD2:
+      Serial.println("SD2");
+      break;
+    case SD_CARD_TYPE_SDHC:
+      Serial.println("SDHC");
+      break;
+    default:
+      Serial.println("Unknown");
+  }
+
+    // print the type and size of the first FAT-type volume
+  uint32_t volumesize;
+  Serial.print("\nVolume type is FAT");
+  Serial.println(volume.fatType(), DEC);
+  Serial.println();
+  
+  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
+  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
+  if (volumesize < 8388608ul) {
+    Serial.print("Volume size (bytes): ");
+    Serial.println(volumesize * 512);        // SD card blocks are always 512 bytes
+  }
+  Serial.print("Volume size (Kbytes): ");
+  volumesize /= 2;
+  Serial.println(volumesize);
+  Serial.print("Volume size (Mbytes): ");
+  volumesize /= 1024;
+  Serial.println(volumesize);
+
+  
+  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
+  root.openRoot(volume);
+  
+  // list all files in the card with date and size
+  root.ls(LS_R | LS_DATE | LS_SIZE);
 
 }
 
